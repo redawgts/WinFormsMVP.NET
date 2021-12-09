a0af4c70416437622331c986961f5af6796e64b5
Joshua Camara <redawgts@gmail.com>
2021-12-09 17:43:44 -0500

    Add Command and Command<T> class

---
+++ src/WinFormsMVP.NET/Command.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WinFormsMVP.NET
+{
+    public abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public virtual bool CanExecute(object parameter) => true;
+
+        public abstract void Execute(object parameter);
+    }
+
+    public abstract class Command<T> : ICommand where T : IView
+    {
+        protected Command(T view)
+        {
+            View = view;
+        }
+
+        protected T View { get; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public virtual bool CanExecute(object parameter) => true;
+
+        public abstract void Execute(object parameter);
+    }
+}
