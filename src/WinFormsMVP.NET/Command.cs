3d2ac16396d96274825baabb72d6cab5320fb79a
Joshua Camara <redawgts@gmail.com>
2021-12-13 18:17:53 -0500

    Updated Version In Projects

---
+++ src/WinFormsMVP.NET/Command.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace WinFormsMVP.NET;
+
+public abstract class Command : ICommand
+{
+    public event EventHandler CanExecuteChanged;
+
+    private void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+    public virtual bool CanExecute(object parameter) => true;
+
+    public abstract void Execute(object parameter);
+}
+
+public abstract class Command<T> : ICommand where T : IView
+{
+    protected T View { get; set; }
+
+    protected Command(T view) => View = view;
+
+    public event EventHandler CanExecuteChanged;
+
+    private void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+    public virtual bool CanExecute(object parameter) => true;
+
+    public abstract void Execute(object parameter);
+}
